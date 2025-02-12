f44cad36b4f299f74d5018848dae45bc4879dace
Asef00 <asefhosseini00@gmail.com>
2025-02-12 14:45:54 +0330

    feat: create see more link component

---
+++ app/components/ui/SeeMoreLink.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+interface SeeMoreLinkProps {
+  href: string
+  className?: string
+}
+
+export default function SeeMoreLink({
+  href,
+  className = '',
+}: SeeMoreLinkProps) {
+  return (
+    <Link
+      href={href}
+      className={`inline-block mt-8 text-orange-500 hover:text-orange-600 transition-colors ${className}`}
+    >
+      See more
+    </Link>
+  )
+}
