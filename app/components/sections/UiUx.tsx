2f6086e59ccfd6c62a1e51e4a0a990e28f0f669e
Asef00 <asefhosseini00@gmail.com>
2025-02-12 14:46:09 +0330

    feat: add ui/ux section content

--- app/components/sections/UiUx.tsx
+++ app/components/sections/UiUx.tsx
@@ -1,28 +1,22 @@
 'use client'
 
 import SectionLayout from '../layouts/SectionLayout'
+import SeeMoreLink from '../ui/SeeMoreLink'
 
 export default function UiUx() {
   const leftContent = (
     <>
-      <p>Project descriptions, text content...</p>
-      {/* Add more content as needed */}
-    </>
-  )
-
-  const rightContent = (
-    <>
-      {/* Add your right column content */}
-      Images, galleries, or other visual content...
+      <h1 className="text-section-heading">UI / UX Design</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip e
+      </p>
+      <SeeMoreLink href="/" />
     </>
   )
 
   return (
-    <SectionLayout
-      id="uiux"
-      title="UI/UX Design"
-      leftContent={leftContent}
-      rightContent={rightContent}
-    />
+    <SectionLayout id="uiux" title="UI/UX Design" leftContent={leftContent} />
   )
-} 
+}
