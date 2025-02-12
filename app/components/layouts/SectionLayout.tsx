4927db24ac43108545a56f68ce69f2fb69cb5eb7
Asef00 <asefhosseini00@gmail.com>
2025-02-12 14:30:07 +0330

    style: make title optional in secion layout

--- app/components/layouts/SectionLayout.tsx
+++ app/components/layouts/SectionLayout.tsx
@@ -1,33 +1,37 @@
 'use client'
 
 interface SectionLayoutProps {
-  id: string;
-  title: string;
-  leftContent: React.ReactNode;
-  rightContent: React.ReactNode;
+  id: string
+  title?: string
+  leftContent: React.ReactNode
+  rightContent: React.ReactNode
 }
 
-export default function SectionLayout({ id, title, leftContent, rightContent }: SectionLayoutProps) {
+export default function SectionLayout({
+  id,
+  title,
+  leftContent,
+  rightContent,
+}: SectionLayoutProps) {
   return (
-    <section id={id} className="min-h-screen flex items-center justify-center py-20">
+    <section
+      id={id}
+      className="min-h-screen flex items-center justify-center py-20"
+    >
       <div className="container mx-auto px-6">
         <div className="flex flex-col md:flex-row md:space-x-12 space-y-8 md:space-y-0">
           {/* Left Column */}
-          <div className="w-full md:w-1/2">
-            <h2 className="text-4xl font-bold mb-8">{title}</h2>
-            <div className="space-y-4">
-              {leftContent}
-            </div>
+          <div className="w-full md:w-1/2 flex justify-center flex-col">
+            {title && <h2 className="text-4xl font-bold mb-8">{title}</h2>}
+            <div className="space-y-4">{leftContent}</div>
           </div>
 
           {/* Right Column */}
-          <div className="w-full md:w-1/2">
-            <div className="h-full">
-              {rightContent}
-            </div>
+          <div className="w-full md:w-1/2 flex justify-center flex-col">
+            <div className="h-full">{rightContent}</div>
           </div>
         </div>
       </div>
     </section>
   )
-} 
+}
